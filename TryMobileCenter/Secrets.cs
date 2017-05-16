240fcfb9c64cc5744948a17d3e773a2e246e12b9
chrisriesgo <cmriesgo@gmail.com>
2017-05-15 19:58:49 -0500

    updated

--- TryMobileCenter/Secrets.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace TryMobileCenter
-{
-	public static class Secrets
-	{
-		public static string MobileCenterAndroid = "android=;";
-		public static string MobileCenteriOS = "ios=";
-	}
-}
